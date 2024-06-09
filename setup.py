d1caba5611e3d361e41468549c6a6cbe7736c859
Surya dhar <suryadhar217@gmail.com>
2024-06-09 15:58:07 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Suryadhar',
+    author_email= 'Suryadhar217@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
