fc3842b5bc8a4b211bd3b108c0dab172e6c06dd2
Surya dhar <suryadhar217@gmail.com>
2024-06-09 14:52:35 +0530

    update template

---
+++ Template.py
@@ -0,0 +1,40 @@
+import os 
+from pathlib import path 
+import logging 
+
+logging.basicConfig(level = logging.info , format= '[%(asctime)s]: %(message)s:')
+
+
+list_of_files = [
+    "src/__init__.py",
+    "src/run_local.py",
+    "src/helper.py",
+    "model/instruction.txt",
+    "requirements.txt",
+    "setup.py",
+    "main.py",
+    "research/trials.ipynb",
+
+]
+
+
+for file in list_of_files
+
+    file = path(file)
+    filedir,filename = os.path.split(file)
+
+    if filedir != '':
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"create directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(file)or os.path.getsize(file) == 0):
+        with open (file, 'w') as f:
+            pass
+            logging.info(f"Crating empty file:{file}")
+
+    else:
+        logging.info(f"{file} is alredy existing") 
+        
+                   
+
+
